5709189a7ec240f55ec7e780a0c969e25a62111a
Shehzab Ahammad <shehzab@codecraft.co.in>
2022-09-12 15:03:03 +0530

    terraform script updated

--- tests/scripts/workload-container-required/main.tf
+++ tests/scripts/workload-container-required/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     coxedge = {
       version = "0.1"
-      source = "coxedge.com/cox/coxedge"
+      source  = "coxedge.com/cox/coxedge"
     }
   }
 }
@@ -17,19 +17,21 @@ data "coxedge_environments" "test" {
 output "envs" {
   value = data.coxedge_environments.test
 }
+//to import existing wrokload to terraform state file
+//terraform import coxedge_workload.test <workload_id>:<environment_name>:<organization_id>
 
 # Workloads
 resource "coxedge_workload" "test" {
-  name = "demo-container-2"
+  name             = "demo-container-2"
   organization_id  = "<organization_id>"
   environment_name = "demo_env"
-  type = "CONTAINER"
-  image = "bitnami/nginx"
-  specs = "SP-2"
+  type             = "CONTAINER"
+  image            = "bitnami/nginx"
+  specs            = "SP-2"
   deployment {
-    name = "test"
+    name               = "test"
     enable_autoscaling = false
-    pops = ["BTR"]
-    instances_per_pop = 1
+    pops               = ["BTR"]
+    instances_per_pop  = 1
   }
 }
