906ab88b55b1160d2b17f71f8fa173feb2f6200b
Shehzab Ahammad <shehzab@codecraft.co.in>
2023-04-28 10:41:35 +0530

    apikey

--- tests/main.tf
+++ tests/main.tf
@@ -8,13 +8,13 @@ terraform {
 }
 
 provider "coxedge" {
-  key = "GM3COPLOU6nOI12/NZ7HNg=="
+  key = ""
 }
 
 resource "coxedge_workload" "test" {
-  name               = "terr-test4"
-  organization_id    = "b0d424e4-4f78-4cb3-8c7c-26781bea9f7e"
-  environment_name   = "test-backend"
+  name               = "terr-test1"
+  organization_id    = "96a582f8-2301-46f3-8200-3fb6afb64e69"
+  environment_name   = "anothercleanenv"
   type               = "VM"
   image              = "stackpath-edge/ubuntu-2004-focal:v202102241556"
   first_boot_ssh_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDbuOix2IU7SvzJQP61aZ05b2KEYAmTssbC6mrRI/4UZ33BKCPMH039ipw33MyCM3xVHY4n6VshLzExAQLb9qJw7F2yA7shPk1sN9XI1gAestp1IAW/9iF3E8cQyZIV2JUm/ixnRFzlLDNr1gYn8W0XxbW1FJ17QLY4PigEx2WB2LJ28BBOBItY2w8ipWP0ZuDYWVoV8zbWmr/3cdLxp7sNcL6D2MD9t6iYnks2REeGCBtmPaRzuZVgd/g66HmR+614vFAFeT66XBe5HEO2ERSohXMnS8DNSTeRSeN6KcWtEdWMqC5RWkypb+/sNI4WH7SB5TPjrY3jUPSUfpmsZd+WZQM7hG9DTm5mSgMfbVJAF4hSJnN31b/FvuNecDEpaGoTTByS4cZKFNjOxnvZHqtWThN3Y371VsOXMwsSaO1I8v5ylj1YrK86xd4XE3XxFaIFMGfxrM7JcqbmTwhdReivu9+TYKKXvdLzbGIqtD+mjwFQakQj9mWKg+aELdlnrH8= shehzabahammad@DESKTOP-6IH172L"
@@ -24,11 +24,6 @@ resource "coxedge_workload" "test" {
     enable_autoscaling = false
     pops               = ["LAS"]
     instances_per_pop  = 1
-  }
-  network_interfaces {
-    vpc_slug     = "default"
-    ip_families  = "IPv4"
-    is_public_ip = false
   }
   probe_configuration = "LIVENESS_AND_READINESS"
   liveness_probe {
