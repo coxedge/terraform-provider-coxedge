7b7d23691fdda5538ed5d752baae5c2cb851f42e
Shehzab Ahammad <shehzab@codecraft.co.in>
2024-02-19 12:05:21 +0530

    storage resource

--- tests/scripts/compute/storages/resource/attach-workload-to-storage/main.tf
+++ tests/scripts/compute/storages/resource/attach-workload-to-storage/main.tf
@@ -11,7 +11,7 @@ provider "coxedge" {
   key = "[INSERT API KEY HERE]"
 }
 
-resource "coxedge_compute_storage_attach_instance" "attach" {
+resource "coxedge_compute_storage_attach_detach_instance" "attach" {
   organization_id  = "<organization_id>"
   environment_name = "<environment name>"
   storage_id       = "<storage_id>"
